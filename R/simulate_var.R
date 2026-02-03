1c285117616397441f1124c4036ad8cfbe8ac46a
Angela-Frances Ibhade <kiaraibhade@gmail.com>
2026-02-02 21:55:04 -0500

    Added eigenvalue-based stationarity checks for VAR models

--- R/simulate_var.R
+++ R/simulate_var.R
@@ -65,3 +65,38 @@ A2_2_nonstat = matrix(c(0.3, 0.1,
 
 y_var2_stat = simulate_var(list(A2_1_stat, A2_2_stat), T, Sigma)
 y_var2_nonstat = simulate_var(list(A2_1_nonstat, A2_2_nonstat), T, Sigma)
+
+
+# ---- Stationarity check ----
+check_var_stationarity = function(A_list) {
+  # Inputs: A_list = list comprising of all AR coefficient matrices
+  
+  p = length(A_list) # lag order
+  k = nrow(A_list[[1]]) # number of time series variables
+  
+  # Forming companion matrix
+  F = do.call(cbind, A_list)
+  
+  # When p = 1, stationarity only depends on eigenvalues of A_1
+  # otherwise, stationarity depends on eigenvalues of companion matrix
+  # Companion matrix is made up of AR coefficient matrices in top row,
+  # and identity matrices in rest of matrix
+  
+  if (p > 1) { 
+    F = rbind(
+      F,
+      cbind(diag(k * (p - 1)), matrix(0, k * (p - 1), k))
+    )
+  }
+  
+  eig = eigen(F, only.values = TRUE)$values
+  
+  # Outputs: 
+  # a. Eigenvalues of companion matrix
+  # b. Stationary: True or False?
+  
+  list(
+    eigenvalues = eig,
+    stationary = all(Mod(eig) < 1)
+  )
+}
